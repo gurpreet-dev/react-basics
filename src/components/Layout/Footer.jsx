89079cf04d0cbd460b7c2b7cc240e2c7cd7ed45c
Gurpreet Singh <gurpreet778851@gmail.com>
2024-12-19 18:20:28 +0530

    change

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,8 +5,7 @@ function Footer() {
         <footer className="pt-4 my-md-5 pt-md-5 border-top">
             <div className="row">
                 <div className="col-12 col-md">
-                    <img className="mb-2" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="24" height="19" />
-                        <small className="d-block mb-3 text-body-secondary">© 2017–2024</small>
+                    <small className="d-block mb-3 text-body-secondary">© 2017–2024</small>
                 </div>
             </div>
         </footer>
